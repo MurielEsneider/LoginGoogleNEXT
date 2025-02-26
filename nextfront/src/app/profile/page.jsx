41dfe0da4ff820efe6b1f49aab6181dc2bd76a5a
Muriel <eesneider2118@gmail.com>
2025-02-26 16:29:26 -0500

    PhotoPerfil

--- nextfront/src/app/profile/page.jsx
+++ nextfront/src/app/profile/page.jsx
@@ -1,5 +1,7 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../../firebase";
 import { useFavorites } from "../../context/FavoritesContext";
 import EditableProfileImage from "../../components/profile/EditImage";
 import EditableName from "../../components/profile/EditName";
@@ -11,16 +13,28 @@ export default function Profile() {
   const [profileImage, setProfileImage] = useState("/default-profile.png");
   const { favorites } = useFavorites();
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // Actualizamos el estado con la información del usuario
+        setName(user.displayName || "Usuario");
+        setProfileImage(user.photoURL || "/default-profile.png");
+      } else {
+        // Si no hay usuario, dejamos los valores por defecto
+        setName("Usuario");
+        setProfileImage("/default-profile.png");
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   const handleSave = () => {
     console.log("Saving profile data:", { name, profileImage });
   };
 
   return (
     <>
-      {/* Fondo con desenfoque */}
       <div className="background-blur"></div>
-
-      {/* Contenedor principal */}
       <div className="min-h-screen flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-lg bg-black/20 backdrop-blur-lg rounded-xl shadow-lg overflow-hidden">
           <div className="p-8 w-full">
